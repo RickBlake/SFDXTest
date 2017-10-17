42010b911c65cd20344ee49b21a7e2a3df737f96
Rick Blake <richard.blake@hotmail.co.uk>
2017-10-17 13:59:26 +0100

    AccountLocator complete

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -1,8 +1,17 @@
 ({
         doInit : function(component, event) {
             var action = component.get("c.findAll");
-            action.setCallback(this, function(a) {
-              component.set("v.accounts", a.getReturnValue());
+            action.setCallback(this, function(resp) {
+                console.log('accounts');
+                console.log(resp.getReturnValue());
+
+                //Update the componet account list
+                component.set("v.accounts", resp.getReturnValue());
+
+                //Publish account list to event
+                var event = $A.get("e.c:AccountsLoaded");
+                event.setParams({"accounts": resp.getReturnValue()});
+                event.fire();
             });
             $A.enqueueAction(action);
         }
